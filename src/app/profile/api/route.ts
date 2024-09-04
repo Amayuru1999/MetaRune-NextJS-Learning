7adc12e47e212e8de2801c0514d4576c345f1a43
Amayuru1999 <amarasingheau@gmail.com>
2024-09-04 14:24:32 +0530

    feat: added route.ts with Cookies to store data.

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,14 +1,21 @@
 import { type NextRequest}   from "next/server";
-import {headers} from "next/headers";
+import {cookies, headers} from "next/headers";
 
 export async function GET(request: NextRequest){
     const requestHeaders = new Headers(request.headers);
     const headerList=headers();
+
+    cookies().set("resultsPerPage","20");
+    const theme=request.cookies.get("theme");
+
     console.log("🟧 Authorization Header:",requestHeaders.get("Authorization"));
     console.log("🟦 Header List:",headerList.get("Authorization"));
+    console.log("🟡 Cookies:",theme);
+    console.log("✅ Cookies:",cookies().get("resultsPerPage"));
     return new Response("<h1>Profile API Data</h1>",{
         headers: {
-            "Content-Type":"text/html"
+            "Content-Type":"text/html",
+            "Set-Cookie":"theme=dark",
         }
     })
 }
