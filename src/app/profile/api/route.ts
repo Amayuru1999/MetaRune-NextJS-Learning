5aa7a742e49764ec946dc204eb7e8225603bc9fa
Amayuru1999 <amarasingheau@gmail.com>
2024-09-04 14:03:40 +0530

    feat: added route.ts request to log the headers in console.

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,7 +1,10 @@
 import { type NextRequest}   from "next/server";
+import {headers} from "next/headers";
 
 export async function GET(request: NextRequest){
     const requestHeaders = new Headers(request.headers);
-    console.log(requestHeaders.get("Authorization"));
+    const headerList=headers();
+    console.log("🟧 Authorization Header:",requestHeaders.get("Authorization"));
+    console.log("🟦 Header List:",headerList.get("Authorization"));
     return new Response("Profile API data")
 }
